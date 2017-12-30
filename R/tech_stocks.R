c45bda8a75b91cb245d811005e41437c9399da3e
Claus Wilke <wilke@austin.utexas.edu>
2017-12-30 16:44:09 -0600

    added tech_stocks dataset

---
+++ R/tech_stocks.R
@@ -0,0 +1,20 @@
+#' Stock prices of four major tech companies
+#' 
+#' This dataset contains several years of daily closing prices for four major
+#' tech companies, Apple, Google, Microsoft, and Facebook.
+#' 
+#' @examples 
+#' ggplot(tech_stocks, aes(x=date, y=price_indexed, color=ticker)) +
+#'   geom_line() +
+#'   scale_color_manual(values=c("#000000", "#E69F00", "#56B4E9", "#009E73"),
+#'                      name="",
+#'                      breaks=c("FB", "GOOG", "MSFT", "AAPL"),
+#'                      labels=c("Facebook", "Alphabet", "Microsoft", "Apple")) +
+#'   scale_x_date(name="year",
+#'                limits=c(ymd("2012-06-01"), ymd("2017-05-31")),
+#'                expand=c(0,0)) + 
+#'   scale_y_continuous(name="stock price, indexed",
+#'                      limits = c(0, 560),
+#'                      expand=c(0,0)) +
+#'   theme_minimal_hgrid()
+"tech_stocks"
---
+++ data-raw/tech_stocks/tech_stocks.R
@@ -0,0 +1,33 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(stringr)
+library(lubridate)
+library(here)
+
+stocks <- data_frame(company = c("Alphabet", "Apple", "Facebook", "Microsoft"),
+                     ticker = c("GOOG", "AAPL", "FB", "MSFT"))
+
+# read in all at once
+prices <- stocks %>%
+  mutate(filename = paste(str_to_lower(ticker), ".csv", sep="")) %>%
+  mutate(file_contents = map(filename,
+                             ~ read_csv(here("data-raw", "tech_stocks", .),
+                                        col_types = "cdddd-"))) %>%
+  select(-filename) %>%
+  unnest() %>%
+  mutate(date=dmy(Date), price=Close) %>%
+  select(company, ticker, date, price)
+
+index_prices <- prices %>%
+  filter(date == ymd("2012-06-01")) %>%
+  select(-company, -date, index_price = price)
+
+prices <- left_join(prices, index_prices) %>%
+  group_by(ticker) %>%
+  mutate(price_indexed = price/index_price*100)
+
+tech_stock <- prices
+
+devtools::use_data(tech_stock)
