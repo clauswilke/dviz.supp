e132934de9a9c8741359b8ee0f84794bd6a4eef3
Claus Wilke <wilke@austin.utexas.edu>
2018-01-07 15:29:54 -0600

    Added 2-letter codes to US regions.

--- data-raw/US_regions/US_regions.R
+++ data-raw/US_regions/US_regions.R
@@ -1,5 +1,8 @@
 # US regions and divisions downloaded from census.gov here:
 #  https://www2.census.gov/geo/pdfs/maps-data/maps/reference/us_regdiv.pdf
 
+# Two-letter abbreviations taken from here:
+# https://pe.usps.com/text/pub28/28apb.htm
+
 US_regions <- read.csv("data-raw/US_regions/US_regions.csv", stringsAsFactors = FALSE)
 devtools::use_data(US_regions, overwrite = TRUE)
