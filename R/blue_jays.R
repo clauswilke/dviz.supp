8037efa00221809e26455486145749a3294acd47
Claus Wilke <wilke@austin.utexas.edu>
2018-05-31 16:12:03 -0500

    add blue jays dataset

---
+++ R/blue_jays.R
@@ -0,0 +1,4 @@
+#' Body measurements for a sample of blue jays
+#'  
+#' This dataset corresponds to the `BlueJays` dataset from the Stat2Data package.
+"blue_jays"
---
+++ data-raw/blue_jays.R
@@ -0,0 +1,5 @@
+data(BlueJays, package = "Stat2Data")
+
+blue_jays <- BlueJays
+
+devtools::use_data(blue_jays, overwrite = TRUE)
